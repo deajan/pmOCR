f61f13a1ae73df43ada2d2653588d639685124d4
deajan <ozy@netpower.fr>
2016-08-04 10:16:37 +0200

    Bumped version to v1.4-beta

--- pmocr.sh
+++ pmocr.sh
@@ -3,7 +3,7 @@
 PROGRAM="pmocr" # Automatic OCR service that monitors a directory and launches a OCR instance as soon as a document arrives
 AUTHOR="(C) 2015-2016 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr - ozy@netpower.fr"
-PROGRAM_VERSION=1.4-dev
+PROGRAM_VERSION=1.4-beta
 PROGRAM_BUILD=2016080401
 
 ## Debug parameter for service

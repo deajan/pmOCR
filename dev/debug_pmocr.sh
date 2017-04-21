1ae583a15176022d3d540432df3453310b03c0a5
deajan <ozy@netpower.fr>
2017-04-21 19:52:08 +0200

    Rebuilt targets

--- dev/debug_pmocr.sh
+++ dev/debug_pmocr.sh
@@ -3,8 +3,8 @@
 PROGRAM="pmocr" # Automatic OCR service that monitors a directory and launches a OCR instance as soon as a document arrives
 AUTHOR="(C) 2015-2017 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr - ozy@netpower.fr"
-PROGRAM_VERSION=1.5.6
-PROGRAM_BUILD=2017041003
+PROGRAM_VERSION=1.5.7
+PROGRAM_BUILD=2017042101
 
 ## Debug parameter for service
 if [ "$_DEBUG" == "" ]; then
@@ -1268,7 +1268,8 @@ function VerComp () {
 	return
 }
 
-SERVICE_MONITOR_FILE="$RUN_DIR/$PROGRAM.SERVICE-MONITOR.run.$SCRIPT_PID.$TSTAMP"
+# This file must not be cleaned with CleanUp function, hence it's naming scheme is different
+SERVICE_MONITOR_FILE="$RUN_DIR/$PROGRAM.$INSTANCE_ID.$SCRIPT_PID.$TSTAMP.SERVICE-MONITOR.run"
 
 function CheckEnvironment {
 	if [ "$OCR_ENGINE_EXEC" != "" ]; then
